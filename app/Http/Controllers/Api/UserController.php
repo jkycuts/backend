fd8a483eb387a4f2101343f5eab271e739599f2b
jkycuts <jkayecutillas5@gmail.com>
2024-03-13 12:06:39 +0800

    Add Store and Update API and Create UserRequest

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
@@ -22,6 +24,35 @@ class UserController extends Controller
     public function show(string $id)
     {
         return User::findOrFail($id);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+
+        $validated = $request->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        $Users = User::create($validated);
+
+        return $Users;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UserRequest $request, string $id)
+    {
+        $validated = $request->validated();
+
+        $Users = User::findORFail($id);
+
+        $Users->update($validated);
+
+        return $Users;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -30,4 +30,6 @@ Route::delete('/carousel/{id}', [CarouselItemsController::class, 'destroy']);
 
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/{id}', [UserController::class, 'show']);
+Route::post('/user', [UserController::class, 'store']);
+Route::put('/user/{id}', [UserController::class, 'update']);
 Route::delete('/user/{id}', [UserController::class, 'destroy']);
